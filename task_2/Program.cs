5bbe9e5bd64a1c6cb2ca8739d9c28fd54bdef093
Malibu <malibu090@mail.ru>
2022-11-29 16:46:55 +0300

    Неправильное решение 2 задачи

---
+++ task_2/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача №3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+//3 -> Среда
+//5 -> Пятница
+
+Console.Clear();
+Console.Write("Введите число ");
+char 1 = char.Parse(Console.WriteLine("Понедельник"));
+int = 2; 
+Console.WriteLine(2.ToString("Вторник"));
+int 3 = Console.WriteLine(3.ToString("Среда"));
+int 4 = Console.WriteLine(4.ToString("Четверг")); 
+int 5 = Console.WriteLine(5.ToString("Пятница"));
+int 6 = Console.WriteLine(6.ToString("Суббота"));
+int 7 = Console.WriteLine(7.ToString("Воскресенье"));
