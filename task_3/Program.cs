fb73c3a7a8dc762fb4af12ad656ad085fd640958
Malibu <malibu090@mail.ru>
2022-11-29 17:14:04 +0300

    Показывает все целые числа от заданного числа до - заданного числа

---
+++ task_3/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.Write("Введите число ");
+int count = 0;
+int number = int.Parse(Console.ReadLine());
+if (number > 0)
+{
+    count = number*(-1);
+        while (count <= number)
+        {
+            Console.Write($"{count}, ");
+        count++;   
+        }
+}
+else
+{
+    count = number;
+    while (count <=number*(-1))
+    {
+        
+    }
+}
